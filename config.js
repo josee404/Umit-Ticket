3a54d7eb3dd1afed22db6584914555f7950bf322
emanuel lezama <emanuellezama1404@gmail.com>
2024-01-15 08:27:29 -0500

    id string

--- config.js
+++ config.js
@@ -3,5 +3,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Jose1404$'
 export const DB_NAME = process.env.DB_NAME || 'ticketT'
-
 export const DB_PORT = process.env.DB_PORT || 3306
